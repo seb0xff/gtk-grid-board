c514a7e211d672aec41559edda4fa3b1c25b3ceb
seb0xff <contact.git@seb0xff.dev>
2024-12-23 13:04:04 +0100

    Enhance test

--- ggb_test.c
+++ ggb_test.c
@@ -6,6 +6,16 @@ typedef struct _Data {
   GtkWindow *window;
 } Data;
 
+static void click_cb(GtkGestureClick *gesture, guint n_press, gdouble x,
+                     gdouble y, gpointer user_data) {
+  GgbGrid *grid =
+      GGB_GRID(gtk_event_controller_get_widget(GTK_EVENT_CONTROLLER(gesture)));
+  // g_print("Click at %f, %f\n", x, y);
+  int xi, yi;
+  ggb_grid_position_to_index(grid, x, y, &xi, &yi);
+  ggb_grid_toggle_at(grid, xi, yi, TRUE);
+}
+
 static void activate_cb(GtkApplication *app, gpointer user_data) {
   Data *data = (Data *)user_data;
   data->window = (GtkWindow *)gtk_application_window_new(app);
@@ -15,18 +25,21 @@ static void activate_cb(GtkApplication *app, gpointer user_data) {
   GgbGrid *grid = ggb_grid_new();
   gtk_widget_set_hexpand(GTK_WIDGET(grid), FALSE);
   gtk_widget_set_vexpand(GTK_WIDGET(grid), FALSE);
-  ggb_grid_set_size(grid, 20, 20);
+  ggb_grid_set_size(grid, 50, 50);
   ggb_grid_set_accent_guideline_repeat(grid, 10);
   // ggb_grid_set_draw_guidelines(grid, FALSE);
   ggb_grid_set_guideline_width(grid, 0.15);
   ggb_grid_set_cell_spacing(grid, 2);
-  ggb_grid_set_cell_radius(grid, 2);
+  ggb_grid_set_cell_radius(grid, 3);
 
   ggb_grid_toggle_at(grid, 10, 10, TRUE);
   ggb_grid_toggle_at(grid, 10, 11, TRUE);
   ggb_grid_toggle_at(grid, 11, 10, TRUE);
   ggb_grid_toggle_at(grid, 11, 11, TRUE);
-  // ggb_grid_set_size(grid, 40, 40);
+
+  GtkGesture *evk = gtk_gesture_click_new();
+  g_signal_connect(evk, "pressed", G_CALLBACK(click_cb), NULL);
+  gtk_widget_add_controller(GTK_WIDGET(grid), GTK_EVENT_CONTROLLER(evk));
   gtk_window_set_child(data->window, GTK_WIDGET(grid));
   gtk_window_present(GTK_WINDOW(data->window));
 }
@@ -42,5 +55,6 @@ int main(int argc, char *argv[]) {
       gtk_application_new("gridboard.test.app", G_APPLICATION_DEFAULT_FLAGS);
   g_signal_connect(data.app, "activate", G_CALLBACK(activate_cb), &data);
   g_application_run(G_APPLICATION(data.app), 0, NULL);
+  g_object_unref(data.app);
   return 0;
 }
